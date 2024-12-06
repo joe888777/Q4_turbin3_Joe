18f1b21a79991ca81afd78a8dcbd7616429d6167
joeAtl <joe.chen@atlancube.com>
2024-12-07 02:26:24 +0800

    escrow

--- anchor_escrow/programs/anchor_escrow/src/lib.rs
+++ anchor_escrow/programs/anchor_escrow/src/lib.rs
@@ -15,7 +15,17 @@ declare_id!("CpKPsTn1hRraD7auXfKoK1qUEzf2otCq6bVzEdSgg4yB");
 pub mod anchor_escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn make(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        ctx.accounts.deposit(deposit)?;
+        ctx.accounts.save_escrow(seed, receive, &ctx.bumps)
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()
     }
 }
